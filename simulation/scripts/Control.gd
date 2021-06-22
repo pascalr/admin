013628002f1d59c027a49131325f6854d382114b
Dark Krael <dark.krael@gmail.com>
2021-06-21 21:43:43 -0400

    .

---
+++ simulation/scripts/Control.gd
@@ -0,0 +1,24 @@
+extends Control
+
+var output
+var text
+
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_CommandLine_text_entered(new_text):
+	print("Command: " + new_text)
+	if new_text == "exit" || new_text == "quit":
+		get_tree().quit()
+	output = get_node("Panel/Output")
+	get_node("CommandLine").clear() # FIXME: Should be self?
+	text = output.text
+	if !text.empty():
+		text += "\n"
+	text += "> " + new_text
+	output.set_text(text)
