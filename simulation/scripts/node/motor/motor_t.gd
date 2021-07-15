08e9ac0560c1c01d5bdc52f3818b350474be137f
Dark Krael <dark.krael@gmail.com>
2021-07-15 11:22:49 -0400

    .

--- simulation/scripts/node/motor/motor_t.gd
+++ simulation/scripts/node/motor/motor_t.gd
@@ -4,7 +4,7 @@ func _ready():
 	setup(rad2deg(self.rotation.y))
 
 func _update_mesh():
-	self.rotation.y = deg2rad(start_offset + position)
+	self.rotation.y = deg2rad(start_offset - position)
 
 func _process(delta):
 	update_position(delta)
