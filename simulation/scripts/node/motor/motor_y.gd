8813930a2a4ee7ed9d1b54e3aa385226b899c5b2
Dark Krael <dark.krael@gmail.com>
2021-07-19 05:22:30 -0400

    ok y

--- simulation/scripts/node/motor/motor_y.gd
+++ simulation/scripts/node/motor/motor_y.gd
@@ -5,15 +5,16 @@ class_name MotorY
 export var reverse = false
 
 func _ready():
+	#self.translation.y = Globals.start_position_y
 	setup(self.translation.y)
 	self.position = Globals.start_position_y
 	self.destination = Globals.start_position_y
 
 func _update_mesh():
 	if reverse:
-		self.translation.y = start_offset - position + Globals.start_position_y
+		self.translation.y = position
 	else:
-		self.translation.y = start_offset + position - Globals.start_position_y
+		self.translation.y = position
 
 func _process(delta):
 	update_position(delta)
