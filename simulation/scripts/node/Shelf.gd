0fa8267f1aa67a2e7a2b5f02f3c9ba120302d45c
Dark Krael <dark.krael@gmail.com>
2021-07-21 10:48:48 -0400

    .

--- simulation/scripts/node/Shelf.gd
+++ simulation/scripts/node/Shelf.gd
@@ -1,5 +1,18 @@
-extends Node
+extends Spatial
+
+signal shelf_click
 
 class_name Shelf
 
 export var grab_above := true
+
+func _ready():
+	for child in get_children():
+		if child is CollisionObject:
+			var _b = child.connect("input_event", self, "_on_input_event")
+	
+	var _a = self.connect("shelf_click", get_node("/root/Simulation/Cupboard"), "_on_shelf_click")
+
+func _on_input_event(_camera, event, click_position, _click_normal, _shape_idx):
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
+		emit_signal("shelf_click", self, click_position)
