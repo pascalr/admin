f8aa62ef3649d157353033361d7e20de2e72f24c
Dark Krael <dark.krael@gmail.com>
2021-07-24 22:06:10 -0400

    .

--- simulation/scripts/autoload/globals.gd
+++ simulation/scripts/autoload/globals.gd
@@ -5,15 +5,13 @@ extends Node
 
 enum {ACTION_SELECT, ACTION_ADD_JAR, ACTION_PUT_DOWN}
 
-var machine_depth := 7.0*25.4
 var working_shelf_depth := 12.0*25.4
+var trolley_z := working_shelf_depth + 4.438*25.4
 
 var humerus_length := 6.25*25.4 # 158.75
-var forearm_grip_length := (3.0 + 0.125 + 3.125 + 0.75 + 2.375)*25.4 # 238.125
+var forearm_grip_length := 204.0
 var forearm_clear_length := (3.0 + 0.125 + 3.125 + 0.75 + 3.25)*25.4
 var gripper_width := 6.0*25.4
-
-var trolley_z := working_shelf_depth + machine_depth/2.0
 
 # The safe height to move above
 # (The tallest jars mesure 168.275)
--- simulation/scripts/autoload/heda.gd
+++ simulation/scripts/autoload/heda.gd
@@ -12,6 +12,16 @@ onready var food = config.get_node("Foods/farine")
 var current_action : int
 var current_selection
 
+func _ready():
+	# Ugly fix to max sure everything is loaded.
+	yield(get_tree().create_timer(0.2), "timeout")
+	assert(abs(robot.translation.z - Globals.trolley_z) < 0.01)
+	assert(abs(robot.support.translation.y - robot.support.position) < 0.01)
+	assert(abs(robot.trolley.translation.x - robot.trolley.position) < 0.01)
+	assert(abs(robot.wrist.translation.z - Globals.humerus_length) < 0.01)
+	assert(abs(robot.hand.translation.z + Globals.forearm_grip_length) < 0.01)
+	#assert(abs(robot.humerus.))
+
 func error(msg):
 	print("Error: "+str(msg))
 	var window = UI.get_node("ErrorDialog")
