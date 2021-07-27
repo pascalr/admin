6cfff9ba02f28ab79edebe0fbbf50b86c0a9c01e
Dark Krael <dark.krael@gmail.com>
2021-07-26 21:59:31 -0400

    .

--- simulation/scripts/autoload/heda.gd
+++ simulation/scripts/autoload/heda.gd
@@ -16,20 +16,20 @@ func _ready():
 	# Ugly fix to max sure everything is loaded.
 	yield(get_tree().create_timer(0.2), "timeout")
 	
-	core = get_node("/root/Simulation")
-	robot = core.get_node("Robot")
-	config = core.get_node("Config")
-	cupboard = core.get_node("Cupboard")
-	UI = core.get_node("UI")
-
-	jar_format = config.get_node("JarFormats/Big")
-	food = config.get_node("Foods/farine")
+#	core = get_node("/root/Simulation")
+#	robot = core.get_node("Robot")
+#	config = core.get_node("Config")
+#	cupboard = core.get_node("Cupboard")
+#	UI = core.get_node("UI")
+#
+#	jar_format = config.get_node("JarFormats/Big")
+#	food = config.get_node("Foods/farine")
 	
-	assert(abs(robot.translation.z - Globals.trolley_z) < 0.01)
-	assert(abs(robot.support.translation.y - robot.support.position) < 0.01)
-	assert(abs(robot.trolley.translation.x - robot.trolley.position) < 0.01)
-	assert(abs(robot.wrist.translation.z - Globals.humerus_length) < 0.01)
-	assert(abs(robot.hand.translation.z + Globals.forearm_grip_length) < 0.01)
+#	assert(abs(robot.translation.z - Globals.trolley_z) < 0.01)
+#	assert(abs(robot.support.translation.y - robot.support.position) < 0.01)
+#	assert(abs(robot.trolley.translation.x - robot.trolley.position) < 0.01)
+#	assert(abs(robot.wrist.translation.z - Globals.humerus_length) < 0.01)
+#	assert(abs(robot.hand.translation.z + Globals.forearm_grip_length) < 0.01)
 	#assert(abs(robot.humerus.))
 
 func error(msg):
