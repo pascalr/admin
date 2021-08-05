792ae171edb7a2942ecc0d31f3d3e1d904d9b6cf
Dark Krael <dark.krael@gmail.com>
2021-08-04 20:24:30 -0400

    .

--- simulation/scripts/visual/FreelookCamera.gd
+++ simulation/scripts/visual/FreelookCamera.gd
@@ -24,7 +24,7 @@ var _q = false
 var _e = false
 
 func _input(event):
-	if get_node(Heda.COMMAND_LINE).has_focus():
+	if get_node(Heda.COMMAND_LINE).has_focus() or !current:
 		return
 	# Receives mouse motion
 	if event is InputEventMouseMotion:
