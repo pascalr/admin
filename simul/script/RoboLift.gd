0047f11c831ff8cba98b6fc5d14844b628c29ecc
Pascal Rainville <pascal.rainville@gmail.com>
2023-03-07 20:09:35 -0500

    .

---
+++ simul/script/RoboLift.gd
@@ -0,0 +1,180 @@
+class_name RoboLift extends Spatial
+
+# Keyboard state
+var _up = false
+var _down = false
+var _forward = false
+var _left = false
+var _backward = false
+var _right = false
+var _cw = false
+var _ccw = false
+
+# CONTROLS
+# w,a,s,d => forward, left, backward, right
+# q,e => turn ccw, turn cw
+# r,f => up, down
+# arrow_up, arrow_left, arrow_down, arrow_right => robotic arm forward, left, backward, right
+# g,space => toggle grab mode, execute grab or release action
+
+func _input(event):
+
+	# Receives key input
+	if event is InputEventKey:
+		match event.scancode:
+			KEY_R:
+				_up = event.pressed
+			KEY_F:
+				_down = event.pressed
+			KEY_W:
+				_forward = event.pressed
+			KEY_A:
+				_left = event.pressed
+			KEY_S:
+				_backward = event.pressed
+			KEY_D:
+				_right = event.pressed
+			KEY_Q:
+				_ccw = event.pressed
+			KEY_E:
+				_cw = event.pressed
+			KEY_UP:
+				_down = event.pressed
+			KEY_LEFT:
+				_down = event.pressed
+			KEY_DOWN:
+				_down = event.pressed
+			KEY_RIGHT:
+				_down = event.pressed
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if _up:
+		$RoboLiftFork.translate(Vector3(0, 50*delta, 0))
+	if _down:
+		$RoboLiftFork.translate(Vector3(0, -50*delta, 0))
+	if _forward:
+		self.translate(Vector3(0, 0, 50*delta))
+	if _backward:
+		self.translate(Vector3(0, 0, -50*delta))
+	if _left:
+		self.translate(Vector3(50*delta, 0, 0))
+	if _right:
+		self.translate(Vector3(-50*delta, 0, 0))
+	if _cw:
+		self.rotate_y(-0.5*delta)
+	if _ccw:
+		self.rotate_y(0.5*delta)
+
+
+
+#
+#export(float, 0.0, 1.0) var sensitivity = 0.25
+#
+#var has_focus = true
+#
+## Mouse state
+#var _mouse_position = Vector2(0.0, 0.0)
+#var _total_pitch = 0.0
+#
+## Movement state
+#var _direction = Vector3(0.0, 0.0, 0.0)
+#var _velocity = Vector3(0.0, 0.0, 0.0)
+#var _acceleration = 30
+#var _deceleration = -10
+#var _vel_multiplier = 500
+#
+## Keyboard state
+#var _w = false
+#var _s = false
+#var _a = false
+#var _d = false
+#var _q = false
+#var _e = false
+#
+#func _input(event):
+#	if get_node(Heda.COMMAND_LINE).has_focus() or !current:
+#		return
+#	# Receives mouse motion
+#	if event is InputEventMouseMotion:
+#		_mouse_position = event.relative
+#
+#	# Receives mouse button input
+#	if event is InputEventMouseButton:
+#		match event.button_index:
+#			BUTTON_MIDDLE:
+#				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED if event.pressed else Input.MOUSE_MODE_VISIBLE)
+#			#BUTTON_WHEEL_UP: # Increases max velocity
+#			#BUTTON_WHEEL_DOWN: # Decereases max velocity
+#
+#	# Receives key input
+#	if event is InputEventKey:
+#		match event.scancode:
+#			KEY_W, KEY_UP:
+#				_w = event.pressed
+#			KEY_S, KEY_DOWN:
+#				_s = event.pressed
+#			KEY_A, KEY_LEFT:
+#				_a = event.pressed
+#			KEY_D, KEY_RIGHT:
+#				_d = event.pressed
+#			KEY_Q:
+#				_q = event.pressed
+#			KEY_E:
+#				_e = event.pressed
+#
+## Updates mouselook and movement every frame
+#func _process(delta):
+#	_update_mouselook()
+#	_update_movement(delta)
+#
+## Updates camera movement
+#func _update_movement(delta):
+#	# Computes desired direction from key states
+#	_direction = Vector3(_d as float - _a as float, 
+#						 _e as float - _q as float,
+#						 _s as float - _w as float)
+#
+#	# Computes the change in velocity due to desired direction and "drag"
+#	# The "drag" is a constant acceleration on the camera to bring it's velocity to 0
+#	var offset = _direction.normalized() * _acceleration * _vel_multiplier * delta \
+#		+ _velocity.normalized() * _deceleration * _vel_multiplier * delta
+#
+#	# Checks if we should bother translating the camera
+#	if _direction == Vector3.ZERO and offset.length_squared() > _velocity.length_squared():
+#		# Sets the velocity to 0 to prevent jittering due to imperfect deceleration
+#		_velocity = Vector3.ZERO
+#	else:
+#		# Clamps speed to stay within maximum value (_vel_multiplier)
+#		_velocity.x = clamp(_velocity.x + offset.x, -_vel_multiplier, _vel_multiplier)
+#		_velocity.y = clamp(_velocity.y + offset.y, -_vel_multiplier, _vel_multiplier)
+#		_velocity.z = clamp(_velocity.z + offset.z, -_vel_multiplier, _vel_multiplier)
+#
+#		translate(_velocity * delta)
+#
+## Updates mouse look 
+#func _update_mouselook():
+#	# Only rotates mouse if the mouse is captured
+#	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+#		_mouse_position *= sensitivity
+#		var yaw = _mouse_position.x
+#		var pitch = _mouse_position.y
+#		_mouse_position = Vector2(0, 0)
+#
+#		# Prevents looking up/down too far
+#		pitch = clamp(pitch, -90 - _total_pitch, 90 - _total_pitch)
+#		_total_pitch += pitch
+#
+#		rotate_y(deg2rad(-yaw))
+#		rotate_object_local(Vector3(1,0,0), deg2rad(-pitch))
+#
+#func get_object_under_mouse():
+#	var mouse_pos = get_viewport().get_mouse_position()
+#	var ray_from = self.project_ray_origin(mouse_pos)
+#	var RAY_LENGTH=20000
+#	var ray_to = ray_from + self.project_ray_normal(mouse_pos) * RAY_LENGTH
+#	var space_state = get_world().direct_space_state
+#	var selection = space_state.intersect_ray(ray_from, ray_to, [], 2147483647, true, true)
+#	if(selection.size()!=0):
+#		print(selection)
+#	return selection
